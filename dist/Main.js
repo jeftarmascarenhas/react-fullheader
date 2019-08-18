b70d46585eeabae6c5eedec2e55f7ad5958d469f
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-08-18 19:31:15 -0300

    build 1.0.1

--- dist/Main.js
+++ dist/Main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
@@ -8,25 +6,64 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _styles = require('./styles');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var Component = function () {
-    function Component(_ref) {
-        var name = _ref.name;
-        return _react2['default'].createElement(
-            'h1',
-            null,
-            'Hello ',
-            name,
-            '!'
-        );
-    }
+var FullHeader = function FullHeader(_ref) {
+    var title = _ref.title,
+        subtitle = _ref.subtitle,
+        bgColor = _ref.bgColor,
+        textColor = _ref.textColor,
+        font = _ref.font,
+        bgImg = _ref.bgImg,
+        video = _ref.video;
 
-    return Component;
-}();
-
-Component.propTypes = {
-    name: _react.PropTypes.string.isRequired
+    var headerStyleCombined = Object.assign({}, _styles.headerStyle, {
+        backgroundColor: bgColor,
+        color: textColor,
+        fontFamily: font,
+        backgroundImage: 'url(' + String(bgImg) + ')'
+    });
+    var component = _react2['default'].createElement(
+        'header',
+        { style: headerStyleCombined },
+        _react2['default'].createElement(
+            'div',
+            { style: _styles.contentStyle },
+            title && _react2['default'].createElement(
+                'h1',
+                { style: _styles.titleStyle },
+                title
+            ),
+            subtitle && _react2['default'].createElement(
+                'h2',
+                { style: _styles.subtitleStyle },
+                subtitle
+            )
+        ),
+        video && _react2['default'].createElement('video', { style: _styles.videoStyle, src: video, autoPlay: true, muted: true, loop: true })
+    );
+    return component;
 };
 
-exports['default'] = Component;
+FullHeader.propTypes = {
+    title: _propTypes2['default'].string,
+    subtitle: _propTypes2['default'].string,
+    bgColor: _propTypes2['default'].string,
+    bgImg: _propTypes2['default'].string
+};
+
+FullHeader.defaultProps = {
+    bgColor: '#ccc',
+    textColor: '#000',
+    font: 'Arial',
+    bgImg: ''
+
+};
+
+exports['default'] = FullHeader;
---
+++ dist/styles.js
@@ -0,0 +1,39 @@
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var titleStyle = exports.titleStyle = {
+    fontSize: '5rem',
+    lineHeight: '1.5'
+};
+
+var subtitleStyle = exports.subtitleStyle = {
+    fontSize: '2rem',
+    lineHeight: '1.5'
+};
+
+var headerStyle = exports.headerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    width: '100%',
+    height: '100vh',
+    backgroundSize: 'cover'
+};
+
+var contentStyle = exports.contentStyle = {
+    width: '100%',
+    position: 'relative',
+    zIndex: 2
+};
+
+var videoStyle = exports.videoStyle = {
+    minWidth: '100%',
+    minHeight: '100%',
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    width: 'auto',
+    height: 'auto',
+    transform: 'translate(-50%, -50%)'
+};
